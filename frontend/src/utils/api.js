e36b7b3660699a459d10b9e0748db454f8b44d8c
Kesarkumari <kesarkumari2000@gmail.com>
2026-04-27 16:58:57 +0530

    update api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://social-media-analyzer-u575.onrender.com/api',
 });
 
 // ✅ ADD THIS INTERCEPTOR (VERY IMPORTANT)
